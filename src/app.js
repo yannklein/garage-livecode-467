7ac7abfcc57b8c798c080333f28f938c832a193e
yannklein <yann.klein@me.com>
2020-12-12 08:54:01 +0000

    Deploying to gh-pages from  @ cc7577129fdee6b8d1f00f6235c2c9a7fa121cfb 🚀

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
-// TODO: give a badass name to your garage
-const myBadAssGarage = "";
+// import the car functions from car.js
+import { fetchCars, addCar } from "./car";
+
+const myBadAssGarage = "wairudo-supiido";
 // DON'T CHANGE THIS LINE
 document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
 // //////////////////////
 
+// define the url to fetch
+const baseUrl = "https://wagon-garage-api.herokuapp.com/";
+const url = `h${baseUrl}/${myBadAssGarage}/cars`;
+
+// select the 4 form inputs and the button
+const brand = document.querySelector("#brand");
+const model = document.querySelector("#model");
+const plate = document.querySelector("#plate");
+const owner = document.querySelector("#owner");
+const button = document.querySelector("#submit-btn");
+
+// get the car list when page loaded
+fetchCars(url);
+
+// listen to a click on the "Add car" form button
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const newCar = {
+    brand: brand.value,
+    model: model.value,
+    plate: plate.value,
+    owner: owner.value
+  };
+  // add a car to the Garage API
+  addCar(url, newCar);
+});
---
+++ src/car.js
@@ -0,0 +1,50 @@
+// select the car list
+const carsList = document.querySelector(".cars-list");
+
+// display a car in the car list
+const displayCar = (car) => {
+  const item = `
+  <div class="car">
+    <div class="car-image">
+      <img src="http://loremflickr.com/280/280/${car.brand} ${car.model}" />
+    </div>
+    <div class="car-info">
+      <h4>${car.brand} ${car.model}</h4>
+      <p><strong>Owner:</strong>${car.owner}</p>
+      <p><strong>Plate:</strong>${car.plate}</p>
+    </div>
+  </div>`;
+  carsList.insertAdjacentHTML("beforeend", item);
+};
+
+// fetch all the cars from the API
+const fetchCars = (url) => {
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      // clean the car list before adding the new one
+      carsList.innerHTML = "";
+      data.forEach((car) => {
+        // add each car found in tthe garage API to the car list
+        displayCar(car);
+      });
+    });
+};
+
+// add a new car to the garage API
+const addCar = (url, newCar) => {
+  fetch(url, {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newCar)
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      // refresh the car list: fetch again all the cars from the API
+      fetchCars(url);
+    });
+};
+
+
+export { fetchCars, addCar };
